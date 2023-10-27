89165d68070a79fb713e815dc91fe8742b019e9f
yvan-k-m <ykamg020@uottawa.ca>
2023-10-27 14:34:31 -0400

    design3 et design4

--- pointcp/design3/PointCP3.java
+++ pointcp/design3/PointCP3.java
@@ -1,1 +1,23 @@
-//design 3 ( Yvan )
+import java.lang.math.*;
+
+public class PointCP3 {
+    public double x;
+    public double y;
+
+    public PointCP3(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void convertStorageToPolar() {
+
+    }
+}
---
+++ pointcp/design4/PointCP4.java
@@ -0,0 +1,32 @@
+import java.lang.Math.*;
+
+public class PointCP4 {
+    public double x;
+    public double y;
+    public double r;
+    public double o;
+
+    public PointCP4(double x, double y, double r, double o) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        thi.o = o;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double getO() {
+        return o;
+    }
+
+}
