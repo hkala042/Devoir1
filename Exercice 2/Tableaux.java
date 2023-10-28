1bbe8bd7cc6653886f008c687b5ef1edadd702a3
Ahmed094-u <aomar094@uottawa.ca>
2023-10-27 20:55:48 -0400

    Avec le code

--- Exercice 2/Tableaux.java
+++ Exercice 2/Tableaux.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class Tableaux { // 550000000
+public class Tableaux { 
 	 public static  long x=11100000;
 	 static ArrayList<Character> tab1= new ArrayList<Character>();
 	 static LinkedList<Object> tab2= new LinkedList<>();
@@ -11,9 +11,9 @@ public class Tableaux { // 550000000
 		 StringBuilder stringBuilder = new StringBuilder();
 		      // tab1
 		        Random random = new Random();
-		  /*   long startTab1 = System.currentTimeMillis();
+		   long startTab1 = System.currentTimeMillis();
 					for(int i=0;i<x;i++) {
-						// pour generer le caractere
+						// pour generer le caractere et les ajoutés
 					char randomChar = (char) (random.nextInt(26) + 'a');
 						tab1.add(randomChar);
 					}
@@ -22,7 +22,7 @@ public class Tableaux { // 550000000
 		           stringBuilder.append(element);}
 				 String result1 = stringBuilder.toString(); 
 			        long endTab1= System.currentTimeMillis();
-					System.out.println("ArrayList: "+(endTab1 - startTab1)); */
+					System.out.println("ArrayList: "+(endTab1 - startTab1)); 
 					
 			 // tab2
 				 long startTab2 = System.currentTimeMillis();
@@ -37,7 +37,7 @@ public class Tableaux { // 550000000
 				long endTab2= System.currentTimeMillis();
 				System.out.println("LinkedList: "+(endTab2 - startTab2)); 
 			
-		 /*    // tab3
+		     // tab3
 				long startTab3 = System.currentTimeMillis();
 				for(int i=0;i<x;i++) {
 				char randomChar = (char) (random.nextInt(26) + 'a');
@@ -47,7 +47,7 @@ public class Tableaux { // 550000000
 	          stringBuilder.append(element);}
 			 String result3 = stringBuilder.toString();
 		        long endTab3= System.currentTimeMillis();
-				System.out.println("Array: "+(endTab3 - startTab3)); */
+				System.out.println("Array: "+(endTab3 - startTab3)); 
 				
 		    }
 	}
